9c4ac4d4ee42052e1b76a710734fa9c7bab533f0
albert <albertsbredikis@gmail.com>
2024-03-26 19:43:47 +0100

    feat(HomePage): add listing data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
-const HomePage = () => {
- return (
-    <div className="container py-4">
-        Homepage
-    </div>
- )
-}
+import { listings } from '@/api/data/listings';
+import ListingList from '@/components/ListingList';
 
+const HomePage = () => {
+  return (
+    <div className='container py-4'>
+      <ListingList listings={listings} />
+    </div>
+  );
+};
 
 export default HomePage;
