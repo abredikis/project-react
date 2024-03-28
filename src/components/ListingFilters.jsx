80ed1edc0e805f94f2a684031c83634dd30042cc
albert <albertsbredikis@gmail.com>
2024-03-28 13:59:09 +0100

    feat(ListingFilters): implement listing filtering ui

---
+++ src/components/ListingFilters.jsx
@@ -0,0 +1,28 @@
+import { Search } from 'lucide-react';
+import { useState } from 'react';
+
+import { Button, DateRangePicker, Input, Stepper } from '@/components/ui';
+
+const ListingFilters = ({onChange}) => {
+
+    const [search, setSearch] = useState('');
+    const [dates, setDates] = useState();
+    const [guests, setGuests] = useState(0);
+
+    const handleSubmit = () => {
+        onChange({search, dates, guests})
+    }
+
+    return (
+        <div className='flex flex-row items-center justify-center gap-2'>
+            <Input className='w-[400px]' placeholder='Search destinations' value={search} onChange={(e) => setSearch(e.target.value)} />
+            <DateRangePicker minDate={new Date()} placeholder='Add dates' value={dates} onChange={setDates} />
+            <Stepper value={guests} onChange={setGuests} label="guest" />
+            <Button onChange={handleSubmit}>
+                <Search className='h-4 w-4' />
+            </Button>
+        </div>
+    );
+};
+
+export default ListingFilters;
