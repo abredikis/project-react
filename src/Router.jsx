82ce137694728b997ad2bfcbb69f8a87a0d64fdf
albert <albertsbredikis@gmail.com>
2024-04-01 20:20:37 +0200

    feat(Router): implement router

---
+++ src/Router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+const Router = () => <RouterProvider router={router} />;
+
+export default Router;
