8229fedcf58a66aa1b27c608b02693a60def14ef
albert <albertsbredikis@gmail.com>
2024-04-24 11:27:07 +0200

    refactor(HomePage): use listingsSlice for fetching listings data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,52 +1,42 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import DataRenderer from '@/components/DataRendered';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
 import { Separator, Spinner } from '@/components/ui';
-import useFetch from '@/hooks/useFetch';
+import { fetchListings } from '@/state/listings/listingsSlice';
 
 const HomePage = () => {
+  const { listings, error, status } = useSelector((state) => state.listings);
+  const dispatch = useDispatch();
+
   const [filters, setFilters] = useState({
     search: '',
     dates: undefined,
     guests: 0,
   });
 
-  const fetchOptions = useMemo(() => ({ params: filters }), [filters]);
-
-  const {
-    data: listings,
-    error,
-    isLoading,
-  } = useFetch('/api/listings', fetchOptions);
-
   const handleFilters = useCallback((filters) => {
     setFilters(filters);
   }, []);
 
-  const renderListingList = () => {
-    if (isLoading) {
-      return (
-        <div className='flex justify-center'>
-          <Spinner size='sm' />
-        </div>
-      );
-    }
+  const fetchOptions = useMemo(() => ({ params: filters }), [filters]);
 
-    if (error) {
-      return <div className='text-center'>{error}</div>;
-    }
+  useEffect(() => {
+    const request = dispatch(fetchListings(fetchOptions));
 
-    return <ListingList listings={listings} />;
-  };
+    return () => {
+      request.abort();
+    };
+  }, [dispatch, fetchOptions]);
 
   return (
     <div className='container py-4'>
       <div className='mb-8'>
         <ListingFilters onChange={handleFilters} />
         <Separator className='my-4' />
-        <DataRenderer error={error} isLoading={isLoading}>
+        <DataRenderer error={error} isLoading={status === 'loading'}>
           <ListingList listings={listings} />
         </DataRenderer>
       </div>
