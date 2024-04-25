3df32d17b7ffd5262c3e46a43c6378c9f3f48b97
albert <albertsbredikis@gmail.com>
2024-04-25 18:09:29 +0200

    feat(SignInPage): add sign in page initial layout

---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return (
+    <div className='container flex h-screen items-center justify-center py-4'>
+      Sign in form here
+    </div>
+  );
+};
+
+export default SignInPage;
