52c1689494fee3776176da1630af42fce3462311
albert <albertsbredikis@gmail.com>
2024-04-25 17:42:24 +0200

    refactor(App): hide navbar for unauthenticated users

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { Outlet } from 'react-router-dom';
 
 import Devbar from '@/components/Devbar/Devbar';
 
+import { useAuth } from './components/AuthProvider';
 import Navbar from './components/Navbar';
 
 const App = () => {
+  const { token } = useAuth();
   return (
     <>
       <div className='fixed bottom-0 left-0 top-0'>
         <Devbar />
       </div>
       <div className='ml-[700px]'>
-        <Navbar />
+        {token && <Navbar />}
         <Outlet />
       </div>
     </>
